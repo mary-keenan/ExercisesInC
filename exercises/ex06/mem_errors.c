b460d01961c098470ab115f307398e580a74708c
mary-keenan <mary.keenan@students.olin.edu>
2019-03-05 14:23:22 -0500

    valgrind exercise -- passing with no errors

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -15,13 +15,16 @@ void free_anything(int *p) {
 }
 
 int read_element(int *array, int index) {
-    int x = array[index];
-    return x;
+    if (index >= 0) {
+        int x = array[index];
+        return x;
+    }
+    return 0;
 }
 
 int main()
 {
-    int never_allocated;
+    int *never_allocated = malloc(sizeof (int));
     int *free_twice = malloc(sizeof (int));
     int *use_after_free = malloc(sizeof (int));
     int *never_free = malloc(sizeof (int));
@@ -34,11 +37,11 @@ int main()
 
     // but it does bounds-check dynamic arrays
     read_element(array2, -1);
-    read_element(array2, 100);
+    // read_element(array2, 100); // array is of length 100
 
     // and it catches use after free
-    free(use_after_free);
     *use_after_free = 17;
+    free(use_after_free);
 
     // never_free is definitely lost
     *never_free = 17;
@@ -47,10 +50,13 @@ int main()
     // free(&never_allocated);
 
     // but this one doesn't
-    free_anything(&never_allocated);
+    free_anything(never_allocated);
 
     free(free_twice);
-    free(free_twice);
+    // free(free_twice);
+
+    free (never_free);
+    free(array2);
 
     return 0;
 }
